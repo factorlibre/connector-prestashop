5587216891dc8a4c20f70965426fabb60e3effe8
Hugo Santos <hugo.santos@factorlibre.com>
2018-01-11 15:58:34 +0100

    [FIX] Fixed language import from prestashop when two "en" languages are present

--- connector_prestashop/models/res_lang/importer.py
+++ connector_prestashop/models/res_lang/importer.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
-
+import logging
+from openerp.addons.connector.unit.backend_adapter import BackendAdapter
 from ...unit.direct_binder import DirectBinder
 from ...backend import prestashop
+from openerp import _, exceptions
+
+_logger = logging.getLogger(__name__)
 
 
 @prestashop
@@ -15,7 +19,11 @@ class LangImporter(DirectBinder):
     ]
 
     def _compare_function(self, ps_val, erp_val, ps_dict, erp_dict):
-        if len(erp_val) >= 2 and len(ps_val) >= 2 and \
-                erp_val[0:2].lower() == ps_val[0:2].lower():
-            return True
+        if len(erp_val) >= 2 and len(ps_val) >= 2:
+                if len(ps_val) == 2:
+                    if erp_val[0:2].lower() == ps_val[0:2].lower():
+                        return True
+                elif len(ps_val) == 4:
+                    if ps_val.replace('-', '_').lower() == erp_val.lower():
+                        return True
         return False
